05201feee06d9390a7da201f5bf5da051984be37
buzzkillb <buzzkillb@protonmail.com>
2026-06-26 16:53:44 -0700

    Remove helper scripts

--- check_cache.py
+++
@@ -1,4 +0,0 @@
-import sqlite3
-db = sqlite3.connect(r'C:\Users\travanx\Dropbox\projects\poe2\ninja\prices.db')
-for row in db.execute("SELECT key, name, chaos_value, divine_value, exalted_value, listing_count, detail_json FROM prices WHERE kind='waystone' ORDER BY rowid DESC LIMIT 10"):
-    print(row)
